9dd68ca97461c5d6299746e9eb3fddc91d53a43a
germanomoraes <61243481+germanomoraes@users.noreply.github.com>
2026-02-07 23:44:32 -0300

    Delete backend directory

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const UsuarioController = require('../controllers/UsuarioController');
-
-const router = express.Router();
-
-router.post('/login', UsuarioController.login);
-router.post('/register', UsuarioController.register);
-
-module.exports = router;
--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-require('./config/database'); // Inicializa o banco
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
